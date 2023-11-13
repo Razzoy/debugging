2a0074f34d0937f58a8a66a7d51e3ad6130563e9
Razzoy <1081892@ucn.dk>
2023-11-13 12:24:43 +0100

    Final.iteration1
    should be done

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,5 +1,7 @@
 // write cool JS here!!
-const myInfo = 'en variabel';
+const myWarning = 'Jeg er en advarsel!'
+const myError = 'Jeg er en fejl!!!'
+const myInfo = 'Jeg er en konsol info: En variabel';
 const myBooks = ["The Catcher in the Rye",
     "To Kill a Mockingbird",
     "1984",
@@ -10,4 +12,14 @@ const myBooks = ["The Catcher in the Rye",
     "The Lord of the Rings",
     "The Da Vinci Code",
     "The Hunger Games"]
+
+console.clear();
+console.time();
+console.group('Konsol Opgave');
+console.log('Jeg er en almindelig konsol log');
+console.info(myInfo);
+console.warn(myWarning);
+console.error(myError);
+console.table(myBooks);
+console.timeEnd();
 
